9453a8932cfa8883991e1be0e5a79f7c2ed7f92d
Emilie <milogare@yahoo.fr>
2024-05-24 23:36:56 +0200

    First commit - Initiated project with dependencies Spring Web, Spring Security, Spring Data JPA, MySQL Driver, Lombok

---
+++ chatopback/src/test/java/com/openclassrooms/chatopback/ChatopbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.chatopback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatopbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
