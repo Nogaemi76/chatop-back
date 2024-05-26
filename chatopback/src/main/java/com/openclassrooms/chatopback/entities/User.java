1523b74bb7e2343a83399d5c9e216da84b586472
Emilie <milogare@yahoo.fr>
2024-05-26 18:34:30 +0200

    Added user entity & repository

---
+++ chatopback/src/main/java/com/openclassrooms/chatopback/entities/User.java
@@ -0,0 +1,42 @@
+package com.openclassrooms.chatopback.entities;
+
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false)
+	private long id;
+
+	@Column(unique = true, nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String password;
+
+	@CreationTimestamp
+	@Column(updatable = false, name = "created_at")
+	private Date createdAt;
+
+	@UpdateTimestamp
+	@Column(name = "updated_at")
+	private Date updatedAt;
+}
