b4b623e7297b2da832d96b8b1cce92f3632cef88
Emilie <milogare@yahoo.fr>
2024-05-30 01:47:11 +0200

    Added ModelMapper dependency & implemented UserDto in auth controller

--- chatopback/src/main/java/com/openclassrooms/chatopback/controllers/AuthenticationController.java
+++ chatopback/src/main/java/com/openclassrooms/chatopback/controllers/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.openclassrooms.chatopback.controllers;
 
 import java.text.SimpleDateFormat;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.openclassrooms.chatopback.dtos.LoginUserDto;
 import com.openclassrooms.chatopback.dtos.RegisterUserDto;
+import com.openclassrooms.chatopback.dtos.UserDto;
 import com.openclassrooms.chatopback.entities.User;
 import com.openclassrooms.chatopback.responses.TokenResponse;
 import com.openclassrooms.chatopback.responses.UserResponse;
@@ -28,9 +30,13 @@ public class AuthenticationController {
 
 	private final AuthenticationService authenticationService;
 
-	public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+	private final ModelMapper modelMapper;
+
+	public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService,
+			ModelMapper modelMapper) {
 		this.jwtService = jwtService;
 		this.authenticationService = authenticationService;
+		this.modelMapper = new ModelMapper();
 	}
 
 	@PostMapping("/register")
@@ -64,20 +70,35 @@ public class AuthenticationController {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
 		User currentUser = (User) authentication.getPrincipal();
+		UserDto currentUserDto = convertToDto(currentUser);
 
 		UserResponse userResponse = new UserResponse();
-		userResponse.setId(currentUser.getId());
-		userResponse.setName(currentUser.getName());
-		userResponse.setEmail(currentUser.getEmail());
+		// userResponse.setId(currentUser.getId());
+		// userResponse.setName(currentUser.getName());
+		// userResponse.setEmail(currentUser.getEmail());
+		userResponse.setId(currentUserDto.getId());
+		userResponse.setName(currentUserDto.getName());
+		userResponse.setEmail(currentUserDto.getEmail());
 
 		// userResponse.setCreatedAt(currentUser.getCreatedAt());
 		// userResponse.setUpdatedAt(currentUser.getUpdatedAt());
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
-		String createdDatedFormatted = simpleDateFormat.format(currentUser.getCreatedAt());
-		String updatedDatedFormatted = simpleDateFormat.format(currentUser.getUpdatedAt());
+
+		// String createdDatedFormatted =
+		// simpleDateFormat.format(currentUser.getCreatedAt());
+		// String updatedDatedFormatted =
+		// simpleDateFormat.format(currentUser.getUpdatedAt());
+		String createdDatedFormatted = simpleDateFormat.format(currentUserDto.getCreatedAt());
+		String updatedDatedFormatted = simpleDateFormat.format(currentUserDto.getUpdatedAt());
+
 		userResponse.setCreatedAt(createdDatedFormatted);
 		userResponse.setUpdatedAt(updatedDatedFormatted);
 
 		return ResponseEntity.ok(userResponse);
+	}
+
+	private UserDto convertToDto(User user) {
+		UserDto userDto = modelMapper.map(user, UserDto.class);
+		return userDto;
 	}
 }
