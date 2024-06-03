cf4fa0d42828a76370ff51d370683b8706e409c6
Emilie <milogare@yahoo.fr>
2024-06-03 23:13:35 +0200

    Completed message controller

--- chatopback/src/main/java/com/openclassrooms/chatopback/controllers/MessageController.java
+++ chatopback/src/main/java/com/openclassrooms/chatopback/controllers/MessageController.java
@@ -1,5 +1,35 @@
 package com.openclassrooms.chatopback.controllers;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.openclassrooms.chatopback.dtos.MessageDto;
+import com.openclassrooms.chatopback.entities.Message;
+import com.openclassrooms.chatopback.services.MessageService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/api/messages")
+@RestController
+@RequiredArgsConstructor
 public class MessageController {
 
+	private final MessageService messageService;
+
+	private final ModelMapper modelMapper;
+
+	@PostMapping("/")
+	ResponseEntity<String> addMessage(@RequestBody MessageDto messageDto) {
+
+		Message message = modelMapper.map(messageDto, Message.class);
+		messageService.saveMessage(message);
+
+		// TODO FIX JSON REPONSE
+		return new ResponseEntity<String>("{\"message\":\"Message sent with success\"}", HttpStatus.OK);
+	}
 }
