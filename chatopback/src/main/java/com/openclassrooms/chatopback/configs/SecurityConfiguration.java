13228e76582e3a4147e9ea094c0e5897f14e20d5
Emilie <milogare@yahoo.fr>
2024-06-21 12:56:54 +0200

    Added PUT in allowedMethods for CorsConfiguration

--- chatopback/src/main/java/com/openclassrooms/chatopback/configs/SecurityConfiguration.java
+++ chatopback/src/main/java/com/openclassrooms/chatopback/configs/SecurityConfiguration.java
@@ -45,7 +45,7 @@ public class SecurityConfiguration {
 		CorsConfiguration configuration = new CorsConfiguration();
 
 		configuration.setAllowedOrigins(List.of("http://localhost:3001", "http://localhost:4200"));
-		configuration.setAllowedMethods(List.of("GET", "POST"));
+		configuration.setAllowedMethods(List.of("GET", "POST", "PUT"));
 		configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
