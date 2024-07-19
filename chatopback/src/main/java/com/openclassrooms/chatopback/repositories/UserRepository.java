e235d8d4e250e547d67fafb7bf481b442a5d47a3
Emilie <milogare@yahoo.fr>
2024-07-20 01:23:19 +0200

    Replaced CrudRepository by JpaRepository in UserRepository

--- chatopback/src/main/java/com/openclassrooms/chatopback/repositories/UserRepository.java
+++ chatopback/src/main/java/com/openclassrooms/chatopback/repositories/UserRepository.java
@@ -2,12 +2,12 @@ package com.openclassrooms.chatopback.repositories;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.openclassrooms.chatopback.entities.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByEmail(String email);
 }
