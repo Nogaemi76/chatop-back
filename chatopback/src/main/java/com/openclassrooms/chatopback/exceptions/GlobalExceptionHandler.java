c132518f8c61acd03ab3a60718f158a088a8b975
Emilie <milogare@yahoo.fr>
2024-07-26 13:56:29 +0200

    Modified custom error responses in GlobalExceptionHandler

--- chatopback/src/main/java/com/openclassrooms/chatopback/exceptions/GlobalExceptionHandler.java
+++ chatopback/src/main/java/com/openclassrooms/chatopback/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.openclassrooms.chatopback.exceptions;
 
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ProblemDetail;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -9,52 +9,53 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.security.SignatureException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(Exception.class)
-	public ProblemDetail handleSecurityException(Exception exception) {
-		ProblemDetail errorDetail = null;
+	public ResponseEntity<String> handleSecurityException(Exception exception) {
 
-		// TODO send this stack trace to an observability tool
-
-		// TODO ADD CUSTOM RESPONSES
 		exception.printStackTrace();
 
 		if (exception instanceof BadCredentialsException) {
-			errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), exception.getMessage());
-			errorDetail.setProperty("description", "The username or password is incorrect");
 
-//			return errorDetail;
+			return new ResponseEntity<String>("{}", HttpStatus.UNAUTHORIZED);
+		}
+
+		else if (exception instanceof MalformedJwtException) {
+
+			return new ResponseEntity<String>("", HttpStatus.UNAUTHORIZED);
 		}
 
 		else if (exception instanceof AccountStatusException) {
-			errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
-			errorDetail.setProperty("description", "The account is locked");
+
+			return new ResponseEntity<String>("{\"message\":\"The account is locked\"}", HttpStatus.FORBIDDEN);
 		}
 
 		else if (exception instanceof AccessDeniedException) {
-			errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
-			errorDetail.setProperty("description", "You are not authorized to access this resource");
+
+			return new ResponseEntity<String>("", HttpStatus.UNAUTHORIZED);
+		}
+
+		else if (exception instanceof MalformedJwtException) {
+
+			return new ResponseEntity<String>("", HttpStatus.UNAUTHORIZED);
 		}
 
 		else if (exception instanceof SignatureException) {
-			errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
-			errorDetail.setProperty("description", "The JWT signature is invalid");
+
+			return new ResponseEntity<String>("", HttpStatus.UNAUTHORIZED);
 		}
 
 		else if (exception instanceof ExpiredJwtException) {
-			errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), exception.getMessage());
-			errorDetail.setProperty("description", "The JWT token has expired");
+
+			return new ResponseEntity<String>("", HttpStatus.UNAUTHORIZED);
 		}
 
-		else {
-			errorDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), exception.getMessage());
-			errorDetail.setProperty("description", "Unknown internal server error.");
-		}
-
-		return errorDetail;
+		return new ResponseEntity<String>("{\"message\":\"" + exception.getMessage() + "\"}",
+				HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
